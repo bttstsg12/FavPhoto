fec8e44d5a1451337264189516e68e36290cf818
Namuun0 <namuunmyag@gmail.com>
2020-06-19 16:30:41 +0800

    photo html

--- photo.js
+++ photo.js
@@ -1,19 +1,30 @@
-console.log('connected');
-
-firebase.database().ref('todos').on('value', snapshot => {
-    todo.innerHTML = ' ';
-    const data = snapshot.val();
-
+var firebaseConfig = {
+    apiKey: "AIzaSyAZHD-1TArFeM0CMMc8GhjDhkK_e5Dem0Q",
+    authDomain: "favphoto-ad87d.firebaseapp.com",
+    databaseURL: "https://favphoto-ad87d.firebaseio.com",
+    projectId: "favphoto-ad87d",
+    storageBucket: "favphoto-ad87d.appspot.com",
+    messagingSenderId: "1000109153430",
+    appId: "1:1000109153430:web:80152b99998dfdabc3b6b7",
+    measurementId: "G-LRM8XZ42Y5"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const imgContainer = document.getElementById('images');
+db.ref("photos").on("value", snap => {
+    for (let i = 0; i < snap.val().length; i++) {
+        const temp = document.createElement('div')
+        temp.style.display = "flex"
+        temp.style.width = '25%'
+        temp.style.flexDirection = "column"
+        var image = document.createElement('img')
+        image.style.width = "100%"
+        image.src = snap.val()[i].url;
+        // document.body.appendChild(image);
+        temp.appendChild(image)
+        const btn = document.createElement('button');
+        btn.innerHTML = "fav"
+        temp.appendChild(btn)
+        imgContainer.appendChild(temp)
+    }
 })
-
-const CreatItem = (text, uniqueId) => {
-    // const div = document.createElement('div');
-    // const img = document.createElement('img')
-    // img.src = "url"
-    // img.style.width = ""
-    // div.appendChild(img)
-    // const div1 = document.createElement('div');
-    // div.appendChild(div1);
-    // const btn = button
-    // div1.appendChild(btn)
-}
--- public/login.js
+++ public/login.js
@@ -1,35 +1,34 @@
-
 var firebaseConfig = {
-  apiKey: "AIzaSyAZHD-1TArFeM0CMMc8GhjDhkK_e5Dem0Q",
-  authDomain: "favphoto-ad87d.firebaseapp.com",
-  databaseURL: "https://favphoto-ad87d.firebaseio.com",
-  projectId: "favphoto-ad87d",
-  storageBucket: "favphoto-ad87d.appspot.com",
-  messagingSenderId: "1000109153430",
-  appId: "1:1000109153430:web:80152b99998dfdabc3b6b7",
-  measurementId: "G-LRM8XZ42Y5"
+    apiKey: "AIzaSyAZHD-1TArFeM0CMMc8GhjDhkK_e5Dem0Q",
+    authDomain: "favphoto-ad87d.firebaseapp.com",
+    databaseURL: "https://favphoto-ad87d.firebaseio.com",
+    projectId: "favphoto-ad87d",
+    storageBucket: "favphoto-ad87d.appspot.com",
+    messagingSenderId: "1000109153430",
+    appId: "1:1000109153430:web:80152b99998dfdabc3b6b7",
+    measurementId: "G-LRM8XZ42Y5"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-var provider = new firebase.auth.GoogleAuthProvider();  
+var provider = new firebase.auth.GoogleAuthProvider();
 
 const login = () => {
-  firebase.auth().signInWithPopup(provider).then(function (result) {
-    var token = result.credential.accessToken;
-    var user = result.user;
-    console.log(user)
-  }).catch(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.log(error);
-  });
-  // window.location.href = "photo.html";
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log(user)
+    }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(error);
+    });
+    //window.location.href = "photo.html";
 
 }
-firebase.auth().signOut().then(function () {
-  // Sign-out successful.
-}).catch(function (error) {
-  // An error happened.
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+}).catch(function(error) {
+    // An error happened.
 });
